dd33bf7fd0cb2afb041022a2848a699999c49528
Muhammed-Nayeem <mohammednayeem808@gmail.com>
2021-01-22 15:14:55 +0600

    Problem-Two budgetCalculator solved

--- assignment.js
+++ assignment.js
@@ -9,3 +9,20 @@ function kilometerToMeter(kilometer) {
   }
 }
 console.log(kilometerToMeter(5)); //Invoking function and console output for testing purpose;
+
+
+
+// Problem-02: budgetCalculator
+function budgetCalculator(watches, phones, laptops) {
+  //Checking the given values negative or not;
+  if (watches < 0 || phones < 0 || laptops < 0) {
+    return "Your entire value must be positive for each product!";
+  } else {
+    var watchesPrice = watches * 50;
+    var phonesPrice = phones * 100;
+    var laptopsPrice = laptops * 500;
+    var totalPrice = watchesPrice + phonesPrice + laptopsPrice;
+    return totalPrice;
+  }
+}
+console.log(budgetCalculator(2, 2, 3)); //Invoking function and console output for testing purpose;
